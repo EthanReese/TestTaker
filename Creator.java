c3d2e4756bef7d9913af3b739c6da4b5cd18a959
CarsonCummins11 <322844@bsd48.org>
2017-05-27 15:23:18 -0700

    Creator class with the printing method

--- Creator.java
+++ Creator.java
@@ -1,5 +1,26 @@
-package TestTaker;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Creator {
-
+ArrayList<ArrayList<String>> questions = new ArrayList<ArrayList<String>>();
+	public Creator(ArrayList<ArrayList<String>> qq){
+	questions = qq;
+}
+	public String TakeTest(){
+		String ret = "";
+		for (int i = 0; i < questions.size(); i++) {
+			ret+=questions.get(i).get(0)+"\n";
+			List<Integer> randList = new ArrayList<Integer>();
+			randList.add(1);
+			randList.add(2);
+			randList.add(3);
+			randList.add(4);
+			Collections.shuffle(questions);
+			for (int j = 0; j < randList.size(); j++) {
+			ret+=questions.get(i).get(j)+"\n";
+			}
+			}
+		return ret;
+	}
 }
