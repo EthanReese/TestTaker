4a5e0e651fec642612d50f138d08dda7088e71a2
EthanReese <ethan.reese01@gmail.com>
2017-05-27 14:26:15 -0700

    Initial commit

---
+++ Creator.java
@@ -0,0 +1,5 @@
+package TestTaker;
+
+public class Creator {
+
+}
---
+++ TestTaker.java
@@ -0,0 +1,10 @@
+package TestTaker;
+
+public class TestTaker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
