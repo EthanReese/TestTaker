faf1a0f15727b58ad3e20202ad5a6502e0829f1f
Vikram Kashyap <vkashyap737@gmail.com>
2017-05-27 15:12:48 -0700

    Added reader

--- Reader.java
+++ Reader.java
@@ -1,5 +1,45 @@
 package TestTaker;
 
-public class Reader {
+import java.io.*;
+improt java.util.Scanner;
 
+public class Reader {
+	Scanner fr;
+	FileWriter fw;
+	FileWriter scorewriter;
+
+	public Reader(String filename, scorefilename) {
+		try {
+			fw = new FileWriter(filename);
+			fr = new Scanner(new File(filename));
+			scorewriter = new FileWriter(scorefilename);
+		}
+	}
+
+	public ArrayList<ArrayList<String>> getQuestions() {
+		ArrayList<ArrayList<String>> qs = new ArrayList<ArrayList<String>>();
+		boolean inanswers = false;
+		
+		while (fr.hasNextLine()) {
+			ArrayList<String> entry = new ArrayList<String>();
+			
+			for (int i = 0; i < 5; i++) {
+				entry.add(fr.nextLine());
+			}
+			
+			qs.add(entry);
+		}
+		
+		return qs;
+	}
+
+	public void addQuestion(String question, String answers) {
+		fw.append(question);
+		fw.append(answers);
+	}
+	
+	public void addScore(int score) {
+		scorefile.append('\n');
+		scorefile.append(Integer.toString(score));
+	}
 }
